d6aa14cafcca362828e10d57cb6dcda1332a0bc5
none <none@gmail.com>
2020-11-14 22:51:11 +0000

    initial commit from template

---
+++ Pods/Target Support Files/Pods-HBRTips/Pods-HBRTips-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HBRTips : NSObject
+@end
+@implementation PodsDummy_Pods_HBRTips
+@end
---
+++ Pods/Target Support Files/Pods-HBRTipsTests/Pods-HBRTipsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HBRTipsTests : NSObject
+@end
+@implementation PodsDummy_Pods_HBRTipsTests
+@end
